0318fec6061f5842f8153fb4f98f3ce5fa1a757f
GONDALIYA DEV <gondaliyadev71@gmail.com>
2025-08-07 17:19:25 +0530

    Update Login.jsx

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -56,7 +56,7 @@ const Login = () => {
     setIsLoading(true);
     
     try {
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch('https://cravenow-2h68.onrender.com/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
