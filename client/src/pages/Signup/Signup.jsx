9a3c7b93676e0e24b3adb9e339c9b0736467783c
GONDALIYA DEV <gondaliyadev71@gmail.com>
2025-08-07 17:19:01 +0530

    Update Signup.jsx

--- client/src/pages/Signup/Signup.jsx
+++ client/src/pages/Signup/Signup.jsx
@@ -70,7 +70,7 @@ const Signup = () => {
     setIsLoading(true);
     
     try {
-      const response = await fetch('http://localhost:5000/api/signup', {
+      const response = await fetch('https://cravenow-2h68.onrender.com/api/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
